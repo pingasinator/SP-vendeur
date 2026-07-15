304dcfeeee60b55419a2683f5d3a582afa7d3b7d
yanni <yanni.ajil@gmail.com>
2026-07-15 14:37:45 +0200

    i misunderstood the query asked for the commands like a dumbass

--- mvc-sp/api/wCommandeCreerHttp.php
+++ mvc-sp/api/wCommandeCreerHttp.php
@@ -1,5 +1,18 @@
 <?php
 
-if(isset($_POST['test'])){
-    echo $_POST['test'];
+require_once '../include/parametre.php';
+
+$cnx = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+if(isset($_POST['Commande'])){
+
+}
+
+if(isset($_POST['LigneCommande'])){
+    $data = JSON_encode($_POST['LigneCommande']);
+
+    $i = 0;
+    foreach($data as $ligne){
+
+    }
 }
